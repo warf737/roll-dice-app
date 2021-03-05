b6fae3956424af9e44492382bdf6ddd9b074f19b
dvasilchuk <dvasilchuk@phoenixit.ru>
2021-03-05 23:44:10 +0300

    add function for roll dices

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CharacterMain from './components/CharacterMain.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+		CharacterMain,
   }
 }
 </script>
+
+<template>
+	<div id="app">
+		<character-main/>
+	</div>
+</template>
 
 <style lang="scss">
 #app {
---
+++ src/components/CharacterMain.vue
@@ -0,0 +1,66 @@
+<script>
+import { DICE } from '@/constants';
+
+export default {
+  name: 'HelloWorld',
+	data() {
+  	return {
+  		mainStats: [],
+			health: [],
+
+		}
+	},
+	computed: {
+  	dice() {
+  		return DICE;
+		}
+	},
+	created() {
+  	console.log(this.rollDice(this.dice.d20, 2));
+	},
+	methods: {
+  	rollDice(dice = 20, count = 1 ) {
+  		let rolls = [];
+  		let total = 0;
+  		for (let i = 0; i < count; i++) {
+  			const rollResult = this.getRandomInteger(1, dice);
+  			rolls.push(rollResult);
+  			total += rollResult;
+			}
+			return {
+				rolls: rolls,
+				total: total,
+			};
+		},
+		getRandomInteger(min = 0, max = 1) {
+			const lower = Math.ceil(Math.min(min, max));
+			const upper = Math.floor(Math.max(min, max));
+			return Math.floor(lower + Math.random() * (upper - lower + 1));
+		},
+	},
+}
+</script>
+
+
+<template>
+	<div class="hello">
+		hello
+	</div>
+</template>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
